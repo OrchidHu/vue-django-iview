7826b8f11d71c2e404dfdedd53c8e96e656b6063
OrchidHu <826446178@qq.com>
2019-02-12 17:57:51 +0800

    setting

--- Web/settings.py
+++ Web/settings.py
@@ -146,9 +146,10 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "iviewpage/dist/static")
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/static/appfront/src/assets/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/appfront/src/assets/').replace('\\', '/')
 CORS_ORIGIN_ALLOW_ALL = True
